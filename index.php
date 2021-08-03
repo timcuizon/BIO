249c4830a6776bd6549481df71d815300e8f1234
Tim Cuizon <78065111+timcuizon@users.noreply.github.com>
2021-08-03 06:41:25 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,27 @@
+</html><!DOCTYPE html>
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>BIOGRAPHY </title>
+	<style>
+		@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;500;600&display=swap');
+		* {box-sizing: border-box; margin: 0; padding: 0; font-family: "Poppins";}
+		body {background-color: #BFAB99; color:#40352D;}
+		.myName {font-size: 3.5rem; margin: 4em; text-align: center;}
+		.content p {font-size: 1.5rem; margin-top:1em;}
+	</style>
+<head>
+<body
+	<div class="myName">
+		<?php
+		echo "TIMOTHY WALTER CUIZON"; 
+		?>
+	</div>
+	<div class="content">
+		<p>I am incoming 2nd year college taking BS in Computer Science Major in Application Development at Universty of Makati. I really enjoy <b> developing an application because for me it is my way to make my imagination come true </b>.
+		<p> At first, I did not know what I really want to pursue in college whether I am taking computer science or information technology and fortunately, in UMak there was a program under computer science which is application development. Since I already have a little experience on developing an application back on my OJT days on SHS and I really have a great experience there, so I took it.</p> 
+		<p> Now, even though my course is challenging,<b> I really enjoyed it and I am excited to explore more of it while keeping myself to get better and better</b>.</p> 
+	</div>
+</body>
+</html>
